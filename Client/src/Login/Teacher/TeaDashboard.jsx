3175385a851b6b2fef90eb5155895226b1fbe1cc
KapilSinghMaan <kapilmaan369@gmail.com>
2025-09-13 12:38:28 +0530

    Sixth commit

--- Client/src/Login/Teacher/TeaDashboard.jsx
+++ Client/src/Login/Teacher/TeaDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import logo from '../../assets/images/logo.png'
+import {Link} from 'react-router'
 
 const TeaDashboard = () => {
 
@@ -25,22 +26,26 @@ const TeaDashboard = () => {
             <img src={logo} alt="" height={60}/>
           </div>
           <ul className='list-unstyled fs-3'>
-            <li className='my-3 m-0 d-flex'><a href='Charbhag.com' className='d-flex w-100'><i className='my-3 fa-regular fa-house'></i> <span className='dash-text '>Home</span></a></li>
-            <li className='my-3 d-flex'><i className='my-3 fas fa-clipboard-list'></i> <span className='dash-text '>Create Quiz</span></li>
-            <li className='my-3 d-flex'><i className='my-3 fas fa-list-check'></i> <span className='dash-text '>Manage Quizzes</span></li>
-            <li className='my-3 d-flex'><i className='my-3 fas fa-file-upload'></i> <span className='dash-text '>Submission</span></li>
-            <li className='my-3 d-flex'><i className='my-3 fas fa-chart-line'></i> <span className='dash-text '>Analytics</span></li>
-            <li className='my-3 d-flex'><i className='my-3 fa-regular fa-circle-question'></i> <span className='dash-text '>Question Bank</span></li>
-            <li className='my-3 d-flex'><i className='my-3 fas fa-bell'></i> <span className='dash-text '>Notification</span></li>
-            <li className='my-3 d-flex'><i className='my-3 fas fa-user-gear'></i> <span className='dash-text '>Profile Setting</span></li>
-            <li className='my-3 d-flex'><i className='my-3 fas fa-comments'></i> <span className='dash-text '>Feedback</span></li>
+            <li className='my-3 d-flex'><Link to="/teahome" className='d-flex align-items-center text-decoration-none text-dark w-100'><i className='my-3 fa-regular fa-house'></i> <span className='dash-text '>Home</span></Link></li>
+            <li className='my-3 d-flex'><Link to="/createquiz" className="d-flex align-items-center text-decoration-none text-dark w-100"><i className='my-3 fas fa-clipboard-list'></i> <span className='dash-text '>Create Quiz</span></Link></li>
+            <li className='my-3 d-flex'><Link to="/managequiz" className="d-flex align-items-center text-decoration-none text-dark w-100"><i className='my-3 fas fa-list-check'></i> <span className='dash-text '>Manage Quizzes</span></Link></li>
+            <li className='my-3 d-flex'><Link to="/submission" className="d-flex align-items-center text-decoration-none text-dark w-100"><i className='my-3 fas fa-file-upload'></i> <span className='dash-text '>Submission</span></Link></li>
+            <li className='my-3 d-flex'><Link to="/analytics" className="d-flex align-items-center text-decoration-none text-dark w-100"><i className='my-3 fas fa-chart-line'></i> <span className='dash-text '>Analytics</span></Link></li>
+            <li className='my-3 d-flex'><Link to="/questionbank" className="d-flex align-items-center text-decoration-none text-dark w-100"><i className='my-3 fa-regular fa-circle-question'></i> <span className='dash-text '>Question Bank</span></Link></li>
+            <li className='my-3 d-flex'><Link to="/notification" className="d-flex align-items-center text-decoration-none text-dark w-100"><i className='my-3 fas fa-bell'></i> <span className='dash-text '>Notification</span></Link></li>
+            <li className='my-3 d-flex'><Link to="/profilesetting" className="d-flex align-items-center text-decoration-none text-dark w-100"><i className='my-3 fas fa-user-gear'></i> <span className='dash-text '>Profile Setting</span></Link></li>
+            <li className='my-3 d-flex'><Link to="/feedback" className="d-flex align-items-center text-decoration-none text-dark w-100"><i className='my-3 fas fa-comments'></i> <span className='dash-text '>Feedback</span></Link></li>
           </ul>
 
-              <div className="dash-logout btn fs-3 mt-5 d-flex align-items-center justify-content-center"><i class="fa-solid fa-right-from-bracket"></i> <span>LOGOUT</span></div>
+              <div className="dash-logout btn fs-3 mt-3 d-flex align-items-center justify-content-center"><i class="fa-solid fa-right-from-bracket"></i> <span>LOGOUT</span></div>
 
         </div>
-        <div className="dashboard-right active bg-info m-0">
+        <div className="dashboard-right p-0 active m-0">
           <i className='fas fa-bars fs-1 position-absolute m-3' id='smallbig'></i>
+          <div className="row m-0 dash-right-header d-flex align-items-center justify-content-start">
+            <div className="col-sm-4 p-0"><p className='ms-5 m-0 ps-4 fs-3'>Hello,Your name name</p></div>
+            <div className="col-sm-4 border p-0"><input type="text" className='w-100'/></div>
+          </div>
         </div>
       </div>
     </div>
