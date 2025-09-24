8868660cbf3e487f86156ee4e2f5116bb0581d3d
KapilSinghMaan <kapilmaan369@gmail.com>
2025-09-24 22:27:50 +0530

    tenth commit

--- Client/src/Login/Teacher/TeaHome.jsx
+++ Client/src/Login/Teacher/TeaHome.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { useEffect, useRef } from 'react';
 
 const TeaHome = () => {
+
+    useEffect(() => {
+        let hr = document.querySelector("#hr");
+        let mn = document.querySelector("#mn");
+        let sc = document.querySelector("#sc");
+        const interval = setInterval(() => {
+            let day = new Date();
+
+            let hh = day.getHours() * 30;
+            let mm = day.getMinutes() * 6;
+            let ss = day.getSeconds() * 6;
+
+            hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+            mn.style.transform = `rotateZ(${mm}deg)`;
+            sc.style.transform = `rotateZ(${ss}deg)`;
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <>
             <div className="row m-0 d-flex align-items-center justify-content-center">
@@ -68,9 +89,30 @@ const TeaHome = () => {
                     </div>
                 </div>
                 <div className="dash-home-right my-2 p-0 ps-4">
-                    <div className="dash-clock"></div>
-                    <div className="dash-calendar mt-3"></div>
+                    <div className="clock-container">
+                        <div className="dash-clock">
+
+                            <div className="circle" id="sc" style={{ '--clr': '#04fc43' }}><i></i></div>
+                            <div className="circle circle2" id="mn" style={{ '--clr': '#fee800' }}><i></i></div>
+                            <div className="circle circle3" id="hr" style={{ '--clr': '#ff2972' }}><i></i></div>
+
+                            <span style={{ '--i': 1 }}><b>1</b></span>
+                            <span style={{ '--i': 2 }}><b>2</b></span>
+                            <span style={{ '--i': 3 }}><b>3</b></span>
+                            <span style={{ '--i': 4 }}><b>4</b></span>
+                            <span style={{ '--i': 5 }}><b>5</b></span>
+                            <span style={{ '--i': 6 }}><b>6</b></span>
+                            <span style={{ '--i': 7 }}><b>7</b></span>
+                            <span style={{ '--i': 8 }}><b>8</b></span>
+                            <span style={{ '--i': 9 }}><b>9</b></span>
+                            <span style={{ '--i': 10 }}><b>10</b></span>
+                            <span style={{ '--i': 11 }}><b>11</b></span>
+                            <span style={{ '--i': 12 }}><b>12</b></span>
+                        </div>
+                    </div>
+
                 </div>
+                <div className="dash-calendar mt-3"></div>
             </div>
         </>
     )
